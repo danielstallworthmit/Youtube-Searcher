cc3cc3d49edbd0dc00edde3453de87c9c4de8f2c
danielstallworthmit <danielstallworthmit@gmail.com>
2017-09-01 22:14:00 -0700

    Set up folder structure

--- youtube-search/src/index.js
+++ youtube-search/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './style/index.css';
+import SearchBar from './src/components/search_bar';
 import registerServiceWorker from './registerServiceWorker';
+
+const API_KEY = ''
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
